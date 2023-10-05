a344f717cd83df5588da194d0f9306ed86649aeb
unknown <davidguerraordonez@gmail.com>
2023-10-04 22:49:30 -0500

    first commit

---
+++ src/main/java/com/example/demo/LibrooApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrooApplication.class, args);
+	}
+
+}
